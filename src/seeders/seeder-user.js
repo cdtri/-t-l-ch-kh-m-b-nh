c42c0e4617cc0e6506ab87181f9dd1c46a6f580f
Cao Duc Tri <cdtri1619lqdnt@gmail.com>
2023-03-14 16:25:32 +0700

    create db for project

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -3,9 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'cdtri1619lqdnt@gmail.com',
+      password: '123456789',
+      firstName: 'Tri',
+      lastName: 'Cao',
+      address: 'Ninh Thuan',
+      gender: 1,
+      roleId: 'R1',
+      phonenumber: '0398827408',
+      positionId: 'P1',
+      image: 'test.jpg',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
